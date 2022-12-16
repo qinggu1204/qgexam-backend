74b4c139cd5c15d1abebda45017b0563d804fff0
lamb <lamb@wzyaijijiwaiwai@163.com>
2022-12-16 09:48:13 +0800

    hh

--- qgexam-common/qgexam-common-web/src/main/java/com/qgexam/common/web/base/BaseController.java
+++ qgexam-common/qgexam-common-web/src/main/java/com/qgexam/common/web/base/BaseController.java
@@ -4,9 +4,12 @@ package com.qgexam.common.web.base;
 import com.qgexam.common.core.context.BaseContextHandler;
 
 /**
- * @Author：dcy
- * @Description:
- * @Date: 2021/4/20 16:30
+ * @project qgexam
+ * @description 其中调用 BaseContextHandler 中的方法，获取当前用户的信息、学生信息、教师信息等，
+ * 所有的 Controller 都需要继承该类，以便获取当前用户的信息
+ * @author yzw
+ * @date 2022/12/16 09:46:47
+ * @version 1.0
  */
 public class BaseController {
 
