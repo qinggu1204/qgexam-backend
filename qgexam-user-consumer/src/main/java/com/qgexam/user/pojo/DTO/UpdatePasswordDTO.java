7829dc412260a627939ec30f50542be0934ea682
fuzongjing <202003150904@zjut.edu.cn>
2022-12-16 19:27:43 +0800

    修改密码DTO修改

--- qgexam-user-consumer/src/main/java/com/qgexam/user/pojo/DTO/UpdatePasswordDTO.java
+++ qgexam-user-consumer/src/main/java/com/qgexam/user/pojo/DTO/UpdatePasswordDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+
 /**
  * @author tageshi
  * @date 2022/12/13 2:27
@@ -12,7 +14,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UpdatePasswordDTO {
+    @NotBlank(message = "手机号不能为空")
     private String loginName;
+    @NotBlank(message = "验证码不能为空")
     private String code;
+    @NotBlank(message = "新密码不能为空")
     private String password;
 }
